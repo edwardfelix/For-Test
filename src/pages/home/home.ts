dd44aa46157c5f9f98dc6093cea5e8f6d5e247b4
edwardfelix <edward@entlogics.com>
2018-04-10 15:36:19 +0530

    aboutUs Page added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,8 @@ export class HomePage {
 
   }
 
+  aboutpage()
+  {
+    this.navCtrl.push(AboutPage);
+  }
 }
